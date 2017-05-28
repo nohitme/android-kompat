8a0603851c98f1952843440c18ccd3543d29259f
Eric Lin <nohitme@gmail.com>
2017-05-28 15:41:29 -0500

    Remove ScaleGestureDetectorCompat since calling explicit method should be more readable

--- appkompat/src/main/kotlin/ericlin/info/appkompat/content/view/ScaleGestureDetectorCompat.kt
+++
@@ -1,7 +0,0 @@
-package ericlin.info.appkompat.content.view
-
-import android.support.v4.view.ScaleGestureDetectorCompat
-
-var Any.quickScaleEnabled: Boolean
-    get() = ScaleGestureDetectorCompat.isQuickScaleEnabled(this)
-    set(value) = ScaleGestureDetectorCompat.setQuickScaleEnabled(this, value)
