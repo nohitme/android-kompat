4b3c4565995887c38d95b73a261d13c245cbe983
Eric Lin <nohitme@gmail.com>
2017-05-27 20:27:12 -0500

    Add ConnectivityManagerCompat and TrafficStatsCompat

---
+++ appkompat/src/main/kotlin/ericlin/info/appkompat/content/net/TrafficStatsCompat.kt
@@ -0,0 +1,30 @@
+package ericlin.info.appkompat.content.net
+
+import android.support.v4.net.TrafficStatsCompat
+import java.net.DatagramSocket
+import java.net.Socket
+import java.net.SocketException
+
+/**
+ * Call through [TrafficStatsCompat.tagSocket]
+ */
+@Throws(SocketException::class)
+fun Socket.tagSocketCompat() = TrafficStatsCompat.tagSocket(this)
+
+/**
+ * Call through [TrafficStatsCompat.untagSocket]
+ */
+@Throws(SocketException::class)
+fun Socket.untagSocketCompat() = TrafficStatsCompat.untagSocket(this)
+
+/**
+ * Call through [TrafficStatsCompat.tagDatagramSocket]
+ */
+@Throws(SocketException::class)
+fun DatagramSocket.tagDatagramSocketCompat() = TrafficStatsCompat.tagDatagramSocket(this)
+
+/**
+ * Call through [TrafficStatsCompat.untagDatagramSocket]
+ */
+@Throws(SocketException::class)
+fun DatagramSocket.untagDatagramSocketCompat() = TrafficStatsCompat.untagDatagramSocket(this)
